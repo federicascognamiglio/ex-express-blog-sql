4630322afd760e4c3e40f031ce87a62ebcdeee1e
federicascognamiglio <scognamigliofede99@gmail.com>
2025-01-22 16:21:54 +0100

    feat: milestone 2 completed

--- controllers/posts-controller.js
+++ controllers/posts-controller.js
@@ -1,10 +1,15 @@
 // IMPORTED DATA
 const posts = require("../data/db");
+const connection = require("../data/db")
 
 // CONTROLLERS
 // Index
 const index = (req, res) => {
-    
+    const sql = "SELECT * FROM posts";
+    connection.query(sql, (err, posts) => {
+        if(err) return res.status(500).json({error: "Internal Server error"});
+        res.json({status: "success", data: posts});
+    })
 }
 
 // Show

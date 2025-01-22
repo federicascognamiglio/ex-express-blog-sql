e314f47b1c916f0c5c98e4830384c3b97a1b82e2
federicascognamiglio <scognamigliofede99@gmail.com>
2025-01-22 16:43:35 +0100

    feat: milestone 4 completed

--- controllers/posts-controller.js
+++ controllers/posts-controller.js
@@ -7,27 +7,38 @@ const connection = require("../data/db")
 const index = (req, res) => {
     const sql = "SELECT * FROM `posts`";
     connection.query(sql, (err, posts) => {
-        if(err) return res.status(500).json({error: "Internal Server error"});
-        res.json({status: "success", data: posts});
+        if (err) return res.status(500).json({ error: "Internal Server error" });
+        res.json({ status: "success", data: posts });
     })
 }
 
 // Show
 const show = (req, res) => {
-    
+    const id = req.params.id;
+    const sql = "SELECT * FROM `posts` WHERE id = ?";
+    connection.query(sql, [id], (err, posts) => {
+        if (err) {
+            return res.status(500).json({ error: "Internal Server error" });
+        } else if (posts.lenght === 0) {
+            return res.status(404).json({ error: "Post not found" })
+        } else {
+            res.json({ status: "success", data: posts[0] });
+        }
+    })
 }
-
 
 // Destroy
 const destroy = (req, res) => {
     const id = req.params.id;
     const sql = "DELETE FROM `posts` WHERE id = ?";
     connection.query(sql, [id], (err) => {
-        if (err) return res.status(500).json({error: "Internal Server error"});
-        res.sendStatus(204);
+        if (err) {
+            return res.status(500).json({ error: "Internal Server error" });
+        } else {
+            return res.sendStatus(204);
+        }
     })
 }
-    
 
 // EXPORT
 module.exports = {
